3a099943420326fb5d0503f21927ef37789f703b
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-05-11 17:13:40 -0300

    feat: config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+        type: process.env.TYPEORM_CONNECTION,
+        host: process.env.TYPEORM_HOST,
+        port: process.env.TYPEORM_PORT,
+        username: process.env.TYPEORM_USERNAME,
+        password: process.env.TYPEORM_PASSWORD,
+        database: process.env.TYPEORM_DATABASE,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      } as TypeOrmModuleOptions),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
