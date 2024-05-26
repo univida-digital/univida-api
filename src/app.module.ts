f53bdb0fd86162d5cec819e7bd7046ccdb1d2d1d
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-05-26 01:14:04 -0300

    feat: migrates config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,12 @@ import { UserModule } from './app/user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DonatorModule } from './app/donator/donator.module';
 import { HospitalModule } from './app/hospital/hospital.module';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-        type: process.env.TYPEORM_CONNECTION,
-        host: process.env.TYPEORM_HOST,
-        port: process.env.TYPEORM_PORT,
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      } as TypeOrmModuleOptions),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     AuthModule,
     DonatorModule,
